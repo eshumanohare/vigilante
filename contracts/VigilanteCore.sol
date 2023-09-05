ba01dd0c154cadc54bb738bbfa767a42f7e35b8d
eshumanohare <eshumanohare@gmail.com>
2023-09-05 14:16:01 +0530

    contracts updated

--- contracts/VigilanteCore.sol
+++ contracts/VigilanteCore.sol
@@ -1,4 +1,81 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >0.8.0;
 
-contract VigilanteCore {}
+contract VigilanteCore {
+    uint256 public departmentID;
+    address public official;
+    mapping(uint256 => FIR) private allFIRs;
+    uint256[] public caseIDs;
+
+    struct FIR_reportingOfficer {
+        string name;
+        uint256 badgeID;
+        uint256 departmentID;
+    }
+
+    struct FIR_incident {
+        uint256 date;
+        uint256 time;
+        string location;
+        string description;
+    }
+
+    struct FIR_complainant {
+        string name;
+        string contact;
+        string residentialAddress;
+    }
+
+    struct FIR_suspect {
+        string name;
+        string description;
+        string lastSeenLocation;
+    }
+
+    struct FIR_witness {
+        string name;
+        string contact;
+        string statement;
+    }
+
+    enum Status {
+        UnderInvestigation,
+        Caught,
+        Solved,
+        Closed
+    }
+
+    struct FIR {
+        uint256 caseID;
+        FIR_reportingOfficer officer;
+        FIR_incident incident;
+        FIR_complainant complainant;
+        FIR_suspect[] suspects;
+        FIR_witness[] witnesses;
+        string[] evidences;
+        Status status;
+    }
+
+    constructor(uint256 _departmentID, address _official) public {
+        departmentID = _departmentID;
+        official = _official;
+    }
+
+    function registerFIR(
+        uint256 caseID,
+        FIR_reportingOfficer calldata officer,
+        FIR_incident calldata incident,
+        FIR_complainant calldata complainant,
+        FIR_suspect[] calldata suspects,
+        FIR_witness[] calldata witnesses,
+        string[] calldata evidences
+    ) external OfficialOnly returns (bool) {
+        require(caseIDs[caseID] == 0, "Error: Case ID already registered");
+    }
+
+    // modifiers
+    modifier OfficialOnly() {
+        require(msg.sender == official, "Error: You are not the official");
+        _;
+    }
+}
--- contracts/VigilanteFactory.sol
+++ contracts/VigilanteFactory.sol
@@ -3,4 +3,50 @@ pragma solidity >0.8.0;
 
 import "./VigilanteCore.sol";
 
-contract VigilanteFactory {}
+contract VigilanteFactory {
+    address public creator;
+    mapping(uint256 => address) public allCores;
+
+    event CoreCreated(
+        address indexed official,
+        uint256 indexed departmentID,
+        address indexed creator
+    );
+
+    constructor() public {
+        creator = msg.sender;
+    }
+
+    function creatorCore(
+        uint256 departmentID,
+        address official
+    ) external CreatorOnly returns (address core) {
+        require(official != address(0), "Error: Address != 0x0");
+        bytes32 salt = keccak256(abi.encode(creator, departmentID, official));
+        core = address(new VigilanteCore{salt: salt}(departmentID, official));
+
+        require(core != address(0), "Error: Failed creating core contract");
+        allCores[departmentID] = core;
+        emit CoreCreated(official, departmentID, creator);
+    }
+
+    // modifiers
+    modifier CreatorOnly() {
+        require(msg.sender == creator, "Error: You are not the creator");
+        _;
+    }
+
+    // setters
+    function changeCreator(
+        address newCreator
+    ) external CreatorOnly returns (bool) {
+        require(newCreator != address(0), "Error: Address != 0x0");
+        creator = newCreator;
+        return true;
+    }
+
+    // getters
+    function getCreator() external view returns (address) {
+        return creator;
+    }
+}
